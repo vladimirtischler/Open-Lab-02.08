22a118929291214af004a3f27ca38a5fb16db696
vladimirtischler <tischler.vladimir@gmail.com>
2019-11-07 09:10:51 +0100

    zukm

---
+++ Open-Lab-02.08/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Open_Lab_02._08
+{
+    class Program
+    {
+        public static bool IsEmpty(string str)
+        {
+            return (str == "");
+        }
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            Console.WriteLine(IsEmpty(a));
+        }
+    }
+}
